e970140c3dc4c3fa5d317f6eeb532449f2d22721
Angga Purnama <anggagewor@gmail.com>
2026-07-12 01:41:52 +0700

    update error pas build

--- src/components/ui/BaseEditor.vue
+++ src/components/ui/BaseEditor.vue
@@ -95,7 +95,9 @@ watch(
   () => props.modelValue,
   (val) => {
     if (editor.value && editor.value.getHTML() !== val) {
-      editor.value.commands.setContent(val, false)
+      editor.value.commands.setContent(val, {
+        emitUpdate: false,
+      })
     }
   },
 )
